8c81bab451deb4423139c4ae8b2aafbc8daff9f9
shota <transparent.blue69@gmail.com>
2016-02-28 15:32:35 +0900

    Lチカできる

--- main.c
+++ main.c
@@ -1,44 +1,63 @@
-/* 
- * File:   main.c
- * Author: shota
- *
- * Created on February 22, 2016, 11:29 PM
- */
+/*
+* File:   main.c
+* Author: shota
+*
+* Created on February 22, 2016, 11:29 PM
+*/
 
 // PIC12F1822 Configuration Bit Settings
 
 // 'C' source line config statements
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <xc.h>
 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-// CONFIG1
-#pragma config FOSC = ECH       // Oscillator Selection (ECH, External Clock, High Power Mode (4-32 MHz): device clock supplied to CLKIN pin)
-#pragma config WDTE = ON        // Watchdog Timer Enable (WDT enabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
-#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-#pragma config IESO = ON        // Internal/External Switchover (Internal/External Switchover mode is enabled)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
+// コンフィギュレーション１の設定
+#pragma config FOSC = INTOSC    // 内部ｸﾛｯｸを使用する(INTOSC)
+#pragma config WDTE = OFF       // ｳｵｯﾁﾄﾞｯｸﾞﾀｲﾏｰ無し(OFF)
+#pragma config PWRTE = ON       // 電源ONから64ms後にﾌﾟﾛｸﾞﾗﾑを開始する(ON)
+#pragma config MCLRE = ON      // 外部ﾘｾｯﾄ信号は使用せずにﾃﾞｼﾞﾀﾙ入力(RA5)ﾋﾟﾝとする(OFF)
+#pragma config CP = OFF         // ﾌﾟﾛｸﾞﾗﾑﾒﾓﾘｰを保護しない(OFF)
+#pragma config CPD = OFF        // ﾃﾞｰﾀﾒﾓﾘｰを保護しない(OFF)
+#pragma config BOREN = ON       // 電源電圧降下常時監視機能ON(ON)
+#pragma config CLKOUTEN = OFF   // CLKOUTﾋﾟﾝをRA6ﾋﾟﾝで使用する(OFF)
+#pragma config IESO = OFF       // 外部・内部ｸﾛｯｸの切替えでの起動はなし(OFF)
+#pragma config FCMEN = OFF      // 外部ｸﾛｯｸ監視しない(FCMEN_OFF)
 
-// CONFIG2
-#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-#pragma config PLLEN = ON       // PLL Enable (4x PLL enabled)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+// コンフィギュレーション２の設定
+#pragma config WRT = OFF        // Flashﾒﾓﾘｰを保護しない(OFF)
+#pragma config PLLEN = OFF      // 動作クロックを32MHzでは動作させない(OFF)
+#pragma config STVREN = ON      // スタックがオーバフローやアンダーフローしたらリセットをする(ON)
+#pragma config BORV = HI        // 電源電圧降下常時監視電圧(2.5V)設定(HI)
+#pragma config LVP = OFF        // 低電圧プログラミング機能使用しない(OFF)
 
-
+#define _XTAL_FREQ 16000000
+#define tx RA5 // シリアル通信送信用
+static void pic_init();
 
 void main(void)
 {
-    while(1)
+  tx = 1;
+  //初期設定
+  pic_init();
+  while(1)
   {
-    }
+    tx = 1;
+    __delay_ms(2000);
+    tx = 0;
+    __delay_ms(2000);
+  }
 }
 
+
+static void pic_init(){
+//clock
+OSCCON = 0b11110000; //32MHz(4xPLL=enable FOSC=8MHz OSCSetting=CONFIGbit)
+//IO
+ANSELA= 0x00;	// set all RA to digital pin
+TRISA = 0x00; // RAを全てアウトプット
+PORTA = 0x00;
+}
